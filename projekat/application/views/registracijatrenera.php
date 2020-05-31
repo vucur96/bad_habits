2275b5b4214b18672f87abcab2f2cbc71be75f36
nikoleta994 <terzicnina@gmail.com>
2020-06-01 01:18:28 +0200

    6. Implementacija

---
+++ projekat/application/views/registracijatrenera.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>REGISTRACIJA TRENERA
+                        
+                        
+        </h2>
+        <form name="registracijatrener" action="" method="POST">
+                            <div class="row form-group">
+                                <div class="col-md-6">
+                                    <!-- <label for="fname">First Name</label> -->
+                                    <input type="text" id="fname" class="form-control" placeholder="Vaše ime" required>
+                                        </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-md-6">
+                                    <!-- <label for="fname">First Name</label> -->
+                                    <input type="text" id="fsurname" class="form-control" placeholder="Vaše prezime" required>
+                                        </div>
+                            </div>
+                            
+                            <div class="row form-group">
+                                <div class="col-md-6">
+                                    
+                                    <input type="date" id="datumrodj" class="form-control" placeholder="Datum rodjenja" min="1930-01-01"required>
+                                        </div>
+                                </div>
+                            <div class="row form-group">
+                                <div class="col-md-6">
+                                    <!-- <label for="fname">First Name</label> -->
+                                    <input type="text" id="kurs" class="form-control" placeholder="Kursevi za koji ste specijalizovani"required>
+                                        </div>
+                            </div>
+                          
+                
+							<div class="form-group">
+								<input type="submit" value="Registruj" class="btn btn-primary">
+							</div>
+						</form>		
+		
+    </body>
+</html>
