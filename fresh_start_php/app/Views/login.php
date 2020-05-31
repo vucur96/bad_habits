282bbf8eae4819708a68cf9331b2538c1da156b7
vucur96 <tamaravucur2@gmail.com>
2020-05-31 17:21:56 +0200

    gost.php

---
+++ fresh_start_php/app/Views/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div class="col-md-10 col-md-offset-1 animate-box">
+            <h2>PRIJAVA</h2>
+		<form action="#">
+                    <div class="row form-group">
+			<div class="col-md-6">
+                            <input type="text" id="fname" class="form-control" placeholder="Korisnicko ime"required>
+			</div>
+                    </div>
+                            
+                    <div class="row form-group">
+                        <div class="col-md-6">
+                            <input type="text" id="password" class="form-control" placeholder="Lozinka"required>
+                        </div>
+                    </div>
+                
+                    <div class="form-group">
+                            <button value="Prijavi se" class="btn btn-primary">Prijavi se</button>
+                    </div>
+                    <p>Ako ste novi korisnik nastavite sa <a href="registracija.html">Registracijom</a></p>
+		</form>		
+	</div>
+    </body>
+</html>
