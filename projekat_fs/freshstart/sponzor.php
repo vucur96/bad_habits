a784750f8cd62f1e82262e6c809c405863a4b859
vucur96 <tamaravucur2@gmail.com>
2020-06-01 22:55:37 +0200

    php

--- projekat_fs/freshstart/sponzor.php
+++ projekat_fs/freshstart/sponzor.php
@@ -1,6 +1,25 @@
 
 <body>
 <?php require('header.php'); ?>
+<aside id="colorlib-hero">
+                <div class="flexslider">
+                    <ul class="slides">
+                        <li style="background-image: url(images/img_bg_2.jpg);">
+                            <div class="overlay"></div>
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+                                        <div class="slider-text-inner text-center">
+                                            <h1>Registracija sponzora</h1>
+                                            <h2><span><a href="index.html">Početna</a> | Registracija sponzora</span></h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
 <h2>REGISTRACIJA SPONZORA
                         
                         </h2>
--- projekat_fs/freshstart/trener.php
+++ projekat_fs/freshstart/trener.php
@@ -1,5 +1,24 @@
 <body>
 <?php require('header.php'); ?>
+<aside id="colorlib-hero">
+                <div class="flexslider">
+                    <ul class="slides">
+                        <li style="background-image: url(images/img_bg_2.jpg);">
+                            <div class="overlay"></div>
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+                                        <div class="slider-text-inner text-center">
+                                            <h1>Registracija trenera</h1>
+                                            <h2><span><a href="index.html">Početna</a> | Registracija trenera</span></h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
 						<br>
 						<h2>REGISTRACIJA TRENERA
                         
