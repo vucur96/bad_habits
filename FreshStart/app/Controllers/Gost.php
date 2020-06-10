4e8d95ddf9532cfbb706c1cd26b64996d3ddbada
vucur96 <tamaravucur2@gmail.com>
2020-06-10 17:46:54 +0200

    ci4

--- FreshStart/app/Controllers/Gost.php
+++ FreshStart/app/Controllers/Gost.php
@@ -2,20 +2,21 @@
 
 class Gost extends BaseController
 {
-	public function index()
-	{
-            echo view('sabloni/header');
-            echo view('stranice/index');
-            echo view('sabloni/footer');
-	}
-        
-        
-        public function login()
-	{
-            echo view('sabloni/header');
-            echo view('stranice/login');
-            echo view('sabloni/footer');
-	}
+    protected function prikaz($page,$data) {
+        $data['controller']='Gost';
+        echo view('sabloni/header');
+        echo view("stranice/$page",$data);
+        echo view('sabloni/footer');
+    }
+    
+    public function index(){
+        $this->prikaz('index',[]);
+    }
+
+
+    public function login(){
+        $this->prikaz('login',[]);
+    }
         
 	//--------------------------------------------------------------------
 
