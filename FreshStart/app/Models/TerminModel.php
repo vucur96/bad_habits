aad2e90ced0d6987cdf6a65b14745360041d83fa
vucur96 <tamaravucur2@gmail.com>
2020-06-13 16:19:56 +0200

    ci4

--- FreshStart/app/Models/TerminModel.php
+++ FreshStart/app/Models/TerminModel.php
@@ -5,11 +5,11 @@
     
 class TerminModel extends Model{
     protected $table      = 'termin';
-    protected $primaryKey = 'TerID';
+    protected $primaryKey = 'TerID,TreID';
     
     protected $returnType     = 'object';
     
-    protected $allowedFields = ['TerID','dan','pocetak','kraj', 'KorisnickoIme'];
+    protected $allowedFields = ['TerID','TreID','dan','pocetak','kraj', 'brojkolikovezbaca'];
     
 }
     
